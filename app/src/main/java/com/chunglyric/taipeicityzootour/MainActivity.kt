05280587ae079b80c6d34594259a31eccd108af6
ChuNglyRic <chunglyric@gmail.com>
2023-03-22 14:55:40 +0800

    調整 status bar 與 TopAppBar 的顏色

--- app/src/main/java/com/chunglyric/taipeicityzootour/MainActivity.kt
+++ app/src/main/java/com/chunglyric/taipeicityzootour/MainActivity.kt
@@ -3,19 +3,14 @@ package com.chunglyric.taipeicityzootour
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.chunglyric.taipeicityzootour.ui.TaipeiCityZooTourApp
-import com.chunglyric.taipeicityzootour.ui.theme.TaipeiCityZooTourTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             TaipeiCityZooTourApp()
         }
--- app/src/main/java/com/chunglyric/taipeicityzootour/ui/home/HomeScreen.kt
+++ app/src/main/java/com/chunglyric/taipeicityzootour/ui/home/HomeScreen.kt
@@ -6,19 +6,25 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import com.chunglyric.taipeicityzootour.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
+    val topAppBarState = rememberTopAppBarState()
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(topAppBarState)
+
     Scaffold(
         topBar = {
-            HomeTopAppBar()
+            HomeTopAppBar(topAppBarState = topAppBarState)
         }
     ) { padding ->
         Text(
-            modifier = Modifier.padding(padding),
+            modifier = Modifier
+                .padding(padding)
+                .nestedScroll(scrollBehavior.nestedScrollConnection),
             text = stringResource(id = R.string.reversed)
         )
     }
@@ -28,7 +34,9 @@ fun HomeScreen() {
 @Composable
 private fun HomeTopAppBar(
     topAppBarState: TopAppBarState = rememberTopAppBarState(),
-    scrollBehavior: TopAppBarScrollBehavior? = TopAppBarDefaults.enterAlwaysScrollBehavior(topAppBarState)
+    scrollBehavior: TopAppBarScrollBehavior? = TopAppBarDefaults.enterAlwaysScrollBehavior(
+        topAppBarState
+    )
 ) {
     val title = stringResource(id = R.string.app_name)
     TopAppBar(
