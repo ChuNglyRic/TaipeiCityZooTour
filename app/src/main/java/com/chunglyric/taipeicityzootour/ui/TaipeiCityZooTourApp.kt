a58e267586f2acab40ad0810bf08dc61491fcb81
ChuNglyRic <chunglyric@gmail.com>
2023-03-26 14:42:05 +0800

    加入 Material3 的 ModalNavigationDrawer

--- app/src/main/java/com/chunglyric/taipeicityzootour/ui/TaipeiCityZooTourApp.kt
+++ app/src/main/java/com/chunglyric/taipeicityzootour/ui/TaipeiCityZooTourApp.kt
@@ -1,19 +1,28 @@
 package com.chunglyric.taipeicityzootour.ui
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
 import com.chunglyric.taipeicityzootour.data.AppContainer
 import com.chunglyric.taipeicityzootour.ui.theme.TaipeiCityZooTourTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaipeiCityZooTourApp(
     appContainer: AppContainer
 ) {
     TaipeiCityZooTourTheme {
         val navController = rememberNavController()
-        TaipeiCityZooTourNavGraph(
-            appContainer = appContainer,
-            navController = navController
-        )
+        ModalNavigationDrawer(
+            drawerContent = { ModalDrawerSheet {} },
+            gesturesEnabled = false
+        ) {
+            TaipeiCityZooTourNavGraph(
+                appContainer = appContainer,
+                navController = navController
+            )
+        }
     }
 }
