5cc50e1b6fb4651bbdf891608ba58a28cccd63cf
ChuNglyRic <chunglyric@gmail.com>
2023-03-28 14:36:19 +0800

    產生 api 測試的類別

---
+++ app/src/androidTest/java/com/chunglyric/taipeicityzootour/data/guides/impl/ApiGuidesRepositoryTest.kt
@@ -0,0 +1,20 @@
+package com.chunglyric.taipeicityzootour.data.guides.impl
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class ApiGuidesRepositoryTest {
+
+    @Test
+    fun getAreaGuide() {
+    }
+
+    @Test
+    fun getAnimalGuideCount() {
+    }
+
+    @Test
+    fun getAnimalGuide() {
+    }
+}
