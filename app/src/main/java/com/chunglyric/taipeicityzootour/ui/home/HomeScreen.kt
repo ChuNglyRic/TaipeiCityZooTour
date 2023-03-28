5ccc7028c7539fe2852c8c9d83e24aedc2ac2129
ChuNglyRic <chunglyric@gmail.com>
2023-03-28 19:13:37 +0800

    加入選單點擊提示

--- app/src/main/java/com/chunglyric/taipeicityzootour/ui/home/HomeScreen.kt
+++ app/src/main/java/com/chunglyric/taipeicityzootour/ui/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.chunglyric.taipeicityzootour.ui.home
 
 import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,6 +20,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,12 +38,21 @@ private fun HomeTopAppBar(
     )
 ) {
     val title = stringResource(id = R.string.app_name)
+    val context = LocalContext.current
+    val nothingHere = stringResource(id = R.string.nothing_here)
+
     TopAppBar(
         title = {
             Text(text = title)
         },
         navigationIcon = {
-            IconButton(onClick = {}) {
+            IconButton(onClick = {
+                Toast.makeText(
+                    context,
+                    nothingHere,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }) {
                 Icon(
                     imageVector = Icons.Default.Menu,
                     contentDescription = stringResource(R.string.drawer)
